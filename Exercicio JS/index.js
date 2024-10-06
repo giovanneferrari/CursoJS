8049aae5bcdeadb031f986cc400214da24cdb5d9
Giovanne Ferrari <vanneferrarii@hotmail.com>
2024-10-05 23:26:03 -0300

    aula scrimba

---
+++ Exercicio JS/index.js
@@ -0,0 +1,7 @@
+let welcomeEl = document.getElementById('welcome-el')
+let name = 'Giovanne'
+let greeting = 'Welcome, '
+
+  welcomeEl.innerText = greeting + name
+
+  welcomeEl.innerText += ' 👋'
---
+++ Exercicio JS/script.js
@@ -0,0 +1,7 @@
+let Cliques = 0
+let Conta = document.getElementById('counter')
+
+function contar() {
+    Cliques = Cliques + 1
+    Conta.innerHTML = Cliques
+}
