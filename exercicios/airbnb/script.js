f62079f56a0a206248618f00dbb334b5dfbe4ac0
Giovanne Ferrari <vanneferrarii@hotmail.com>
2024-10-29 22:18:31 -0300

    att airbnb

--- exercicios/airbnb/script.js
+++ exercicios/airbnb/script.js
@@ -1,5 +1,16 @@
 const searchButton = document.querySelector('#search')
+const traco = document.querySelector('#traco')
+const barraPesquisa = document.querySelector('input')
+const ondePesquisa2 = document.querySelector('.barra-pesquisa2')
+const ondePesquisa3 = document.querySelector('.barra-pesquisa3')
+const ondePesquisa4 = document.querySelector('.barra-pesquisa4')
 
 searchButton.addEventListener('click', (event) => {
     alert(`você clicou!`)
+})
+
+barraPesquisa.addEventListener('click', (event) => {
+    traco.style.display = 'none';
+    ondePesquisa2.style.background = '#eeeeee';
+    ondePesquisa2.style.borderRadius = '999px';
 })
